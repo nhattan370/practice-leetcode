2c421813b84e95f69b09d99c98a6ffa2ad0f4bcd
nha khoa <nhattanhmd123@gmail.com>
2025-08-02 14:01:06 +0700

    solve add two number by linked list

---
+++ src/main/java/com/rikkei/AddTwoNumber.java
@@ -0,0 +1,47 @@
+package com.rikkei;
+
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ */
+public class AddTwoNumber {
+      public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode dummy = new ListNode();
+        ListNode pointer = dummy;
+        while(l1!=null || l2!=null){
+            int num1 = (l1!=null) ? l1.val : 0;
+            int num2 = (l2!=null) ? l2.val : 0;
+
+            int sum = num1 + num2 + carry;
+            carry = sum/10;
+
+            pointer.next = new ListNode(sum%10);
+            pointer = pointer.next;
+
+            if(l1!=null) l1 = l1.next;
+            if(l2!=null) l2 = l2.next;
+        }
+        if(carry>0) pointer.next = new ListNode(1);
+        return dummy.next;
+    }
+}
