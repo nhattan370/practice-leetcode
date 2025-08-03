17c61365505dadcb1fe52becffadcce243d2dcd8
nha khoa <nhattanhmd123@gmail.com>
2025-08-03 15:30:54 +0700

    Solve the longest substring without repeat character

--- src/main/java/com/rikkei/Main.java
+++ src/main/java/com/rikkei/Main.java
@@ -4,6 +4,8 @@ package com.rikkei;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        LongestSubstringSlidingWindow longestSubstring = new LongestSubstringSlidingWindow();
+        int a = longestSubstring.lengthOfLongestSubstring(" ");
+        System.out.println(a);
     }
 }
