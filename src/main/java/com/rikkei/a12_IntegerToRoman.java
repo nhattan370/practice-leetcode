a1be7776858634dd20532ae3ca050e42992470a6
nha khoa <nhattanhmd123@gmail.com>
2025-10-21 10:50:24 +0700

    convert integer to roman

---
+++ src/main/java/com/rikkei/a12_IntegerToRoman.java
@@ -0,0 +1,25 @@
+package com.rikkei;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class a12_IntegerToRoman {
+    public String intToRoman(int num) {
+        StringBuilder sbd = new StringBuilder();
+
+        String roman[] = new String[]{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        Integer number[] = new Integer[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        for(int i=0; i<number.length; i++){
+            if(num>0){
+                while(num>=number[i]){
+                    num-=number[i];
+                    sbd.append(roman[i]);
+                }
+            }else{
+                break;
+            }
+        }
+        return sbd.toString();
+    }
+}
